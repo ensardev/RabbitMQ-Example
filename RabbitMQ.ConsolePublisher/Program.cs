ae39e129f503b97f58620b8ba277c725a955e9aa
ensarakkuzey <ensarakkuzey@gmail.com>
2022-06-09 11:52:00 +0300

    Add project files.

---
+++ RabbitMQ.ConsolePublisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQ.ConsolePublisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
